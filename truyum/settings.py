7911e7e996d74f3b977d48a79d9d3306f4c73bf8
NiteshVishwakarma896 <niteshvish896@gmail.com>
2022-05-25 12:50:20 +0530

    Added STATIC_ROOT Urls

--- truyum/settings.py
+++ truyum/settings.py
@@ -117,12 +117,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 #     '/var/www/static/',
 # ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
